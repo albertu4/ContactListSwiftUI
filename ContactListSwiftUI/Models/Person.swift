bc3b4db7a2ba0e63700d5d991eca05b58b841d9d
Miwka <97275239+albertu4@users.noreply.github.com>
2022-06-06 11:01:28 +0900

    Initial commit

---
+++ ContactListSwiftUI/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  ContactListSwiftUI
+//
+//  Created by Михаил Иванов on 06.06.2022.
+//
+
+import Foundation
---
+++ ContactListSwiftUI/Services/DataManager.swift
@@ -0,0 +1,15 @@
+//
+//  DataManager.swift
+//  ContactListSwiftUI
+//
+//  Created by Михаил Иванов on 06.06.2022.
+//
+
+import Foundation
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    private init() {}
+}
