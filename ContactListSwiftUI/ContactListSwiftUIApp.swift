04f7ea2ea865574bbb9ac00b3d1c44a443ff6226
Miwka <97275239+albertu4@users.noreply.github.com>
2022-06-06 10:56:06 +0900

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Михаил Иванов on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
